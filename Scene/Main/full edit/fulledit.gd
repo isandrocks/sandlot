188c464c7c0caee0e515d22b997ad545b5e44f94
iSandRocks <sean.h1313@gmail.com>
2024-08-03 18:02:05 -0400

    feat: Add save_shader function to full edit scene

--- Scene/Main/full edit/fulledit.gd
+++ Scene/Main/full edit/fulledit.gd
@@ -19,6 +19,30 @@ func _ready():
 func _process(_delta):
 	pass
 
+func save_shader():
+	shaderFile_Read = FileAccess.open(shaderFile_path, FileAccess.READ)
+	var save_data = shaderFile_Read.get_as_text()
+	shaderFile_Read.close()	
+
+	var base_name = "saved_shader"
+	var extension = ".gdshader"
+	var save_path = base_name + extension
+	
+	var counter = 1
+	# Check if the file exists and increment the counter until we find a non-existing filename
+	while FileAccess.file_exists(save_path):
+		save_path = base_name + "_" + str(counter) + extension
+		counter += 1
+	
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+
+	if file:
+		file.store_string(save_data)
+		file.close()
+		print("Shader saved to:", save_path)
+	else:
+		print("Failed to save the Shader.")
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scene/Main/M_Menu.tscn")
 
@@ -30,6 +54,10 @@ func _on_text_edit_text_changed():
 		$Timer.start()
 
 func _on_clear_pressed():
+	shaderFile_Read = FileAccess.open(shaderFile_default, FileAccess.READ)
+	var content = shaderFile_Read.get_as_text()
+	shaderFile_Read.close()	
+	$TextEdit.text = content
 	$ShaderControl/ColorRect.material.shader = null
 
 func _on_load_pressed():
@@ -43,3 +71,7 @@ func _on_timer_timeout():
 	var new_shader = Shader.new()
 	new_shader.set_code(content)
 	$ShaderControl/ColorRect.material.shader = new_shader
+
+
+func _on_save_pressed():
+	save_shader()
